04569540259451d54be1a825d2567ed9315bdb83
Denis Bystruev <dbystruev@me.com>
2021-05-05 16:15:27 +0300

    Added Acoola shop

--- Sources/Server/Models/RemoteShop.swift
+++ Sources/Server/Models/RemoteShop.swift
@@ -7,13 +7,35 @@
 import Foundation
 
 struct RemoteShop: Codable {
-    let code: String
-    let currency: String
-    let feed_id: String
-    let format: String
-    let last_import: String
+    let code: String?
+    let currency: String?
+    let feed_id: String?
+    let format: String?
+    let last_import: String?
     let name: String
     let path: String
-    let template: String
-    let user: String
+    let template: String?
+    let user: String?
+    
+    init(
+        code: String? = nil,
+        currency: String? = nil,
+        feed_id: String? = nil,
+        format: String? = nil,
+        last_import: String? = nil,
+        name: String,
+        path: String,
+        template: String? = nil,
+        user: String? = nil
+    ) {
+        self.code = code
+        self.currency = currency
+        self.feed_id = feed_id
+        self.format = format
+        self.last_import = last_import
+        self.name = name
+        self.path = path
+        self.template = template
+        self.user = user
+    }
 }
